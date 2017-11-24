e9522af31ad0498c0d5d2a3c6b2fa068b8e9a6dc
gistrec <gistrec@mail.ru>
2017-11-25 02:23:25 +0700

    Теперь функция connected_components(const Graph &graph) работает :)

--- Работа_6/Работа_6/graph_algs.cpp
+++ Работа_6/Работа_6/graph_algs.cpp
@@ -1,43 +1,62 @@
 #include "graph_algs.h"
 #include <queue>
 
-bool find(std::vector<int> vector, int need) {
+// Поиск числа в векторе
+// Нужна чтобы искать пройдена ли вершина или нет
+bool find(std::vector<int> vector, int verticle) {
 	for (auto elem = vector.begin(); elem != vector.end(); elem++) {
-		if (*elem == need) return true;
+		if (*elem == verticle) return true;
 	}
 	return false;
 }
 
+// Функция для поиска компонент связанности
 std::vector<std::vector<int>> connected_components(const Graph &graph) {
+	// Список всех вершин в графе
 	std::vector<int> verticles = graph.get_vertices();
 
+	// Пройденные вершины
+	// Если вершина есть в векторе - она пройдена
+	// Вершину нужно искать функцией find(std::vector<int> vector, int verticle)
+	std::vector<int> isRun;
+
+	// Результат
 	std::vector<std::vector<int>> result;
 
-	std::vector<int> isRun;
-	std::queue<int> queue;
+	// Дле всех вершин в графе
+	for (auto verticle = verticles.begin(); verticle != verticles.end(); verticle++) {
+		// Если вершина не пройдена
+		if (!find(isRun, *verticle)) {
+			// Создаем новую компоненту связанности
+			std::vector<int> component;
 
-	queue.push(verticles.at(0));
+			// Создаем очередь, чтобы найти все компоненты связанности
+			std::queue<int> queue;
 
-	// Дле всех вершин в очереди
-	for (auto v = verticles.begin(); v != verticles.end(); v++) {
-		std::vector<int> component;
-		while (queue.size() != 0) {
-			// Вершина, которую будем обрабатывать
-			int verticle = queue.front();
-			queue.pop();
-			// Если вершина пройдена
-			if (find(isRun, verticle)) continue;
-			component.push_back(verticle);
-			isRun.push_back(verticle);
+			// Добавляем текущую вершину в очередь
+			queue.push(*verticle);
 
-			std::vector<int> adjacent = graph.get_adjacent_vertices(verticle);
-			for (auto elem = adjacent.begin(); elem != adjacent.end(); elem++) {
-				queue.push(*elem);
+			// Пока очередь не пуста
+			// P.S. Т.е. тут идет поиск всех вершин, которые
+			// Находятся в той же комп. связанности где и вершина *verticle
+			while (!queue.empty()) {
+				// Достаем вершину из очереди
+				int runVerticle = queue.back();
+				queue.pop();
+				// Добавляем вершину в компоненту связанности
+				component.push_back(runVerticle);
+				// Отмечаем текущую вершину как пройденную
+				isRun.push_back(runVerticle);
+				// Получаем смежные вершины
+				std::vector<int> adjacent_vertices = graph.get_adjacent_vertices(runVerticle);
+				// Если вершины не пройдены - добавляем их в очередь
+				for (auto v = adjacent_vertices.begin(); v != adjacent_vertices.end(); v++) {
+					if (!find(isRun, *v)) queue.push(*v);
+				}
 			}
+			result.push_back(component);
 		}
-		if (!component.empty()) result.push_back(component);
 	}
-
 	return result;
 }
 
--- Работа_6/Работа_6/main.cpp
+++ Работа_6/Работа_6/main.cpp
@@ -1,14 +1,23 @@
 #include "graph_algs.h"
-
-//  Привет
+#include "iostream"
 
 void main() {
-	Graph* graph = new Graph();
-	graph->add_vertex(0);
-	graph->add_vertex(1);
-	graph->add_edge(0, 1);
+	Graph graph;
+	graph.add_vertex(0);
+	graph.add_vertex(1);
+	graph.add_vertex(2);
+	graph.add_vertex(3);
+	graph.add_vertex(4);
+	graph.add_edge(0, 1);
+	graph.add_edge(2, 3);
 
-	
+	std::vector<std::vector<int>> result = connected_components(graph);
 
-
+	for (std::vector<std::vector<int>>::iterator iter = result.begin(); iter != result.end(); ++iter) {
+		for (std::vector<int>::iterator innerIter = iter->begin(); innerIter != iter->end(); ++innerIter) {
+			std::cout << *innerIter << " ";
+		}
+		std::cout << std::endl;
+	}
+	system("pause");
 }
