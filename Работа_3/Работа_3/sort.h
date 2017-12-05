20d92cfbf986b6d7290fba9db56a5ddf9f67bb79
gistrec <gistrec@mail.ru>
2017-12-05 11:15:30 +0700

    Добавлены основные файлы

---
+++ Работа_3/Работа_3/sort.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+
+void printArray(std::vector<int> &array) {
+	for (std::vector<int>::iterator iter = array.begin(); iter != array.end(); ++iter) {
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+}
+
+void counting_sort(std::vector<int> &vec) {
+	// —оздаем вспомогательный массив
+	
+	// ћинимальное и максимальное значение
+	
+	std::vector<int> cnt(vec.size());
+	// «аполн€ем вспомогательный массив нул€ми
+	for (auto iter = cnt.begin(); iter != cnt.end(); ++iter) {
+		*iter = 0;
+	}
+	for (auto iter = vec.begin(); iter != vec.end(); ++iter) {
+		++cnt[*iter];
+	}
+	int b = 0;
+
+	for (size_t j = 0; j < cnt.size(); ++j) {
+		for (size_t i = 0; i < cnt[j]; ++i) {
+			vec[b] = j;
+			//std::cout << j << " ";
+			++b;
+		}
+	}
+}
---
+++ Работа_3/Работа_3/sort_test.cpp
@@ -0,0 +1,34 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "sort.h"
+
+TEST_CASE("Empty array") {
+	std::vector<int> input = {};
+	std::vector<int> expected = {};
+	counting_sort(input);
+	CHECK(input == expected);
+}
+
+TEST_CASE("One element") {
+	std::vector<int> input = { 0 };
+	std::vector<int> expected = { 0 };
+	counting_sort(input);
+	CHECK(input == expected);
+}
+
+TEST_CASE("Dual element") {
+	std::vector<int> input = { 0, 0 };
+	std::vector<int> expected = { 0, 0 };
+	counting_sort(input);
+	CHECK(input == expected);
+}
+
+TEST_CASE("Many elements") {
+	std::vector<int> input = { 1, 3, 0, 2 , 4 };
+	std::vector<int> expected = { 0, 1, 2, 3 , 4 };
+	// Результат будет записан на место входного массива
+	counting_sort(input);
+	// Проверка результата и ожидаемого результата на равенство 
+	CHECK(input == expected);
+}
