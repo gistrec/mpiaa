eff5b8f12d3d7a35003dcfff80ce315ea34dd48a
gistrec <gistrec@mail.ru>
2017-12-07 19:00:38 +0700

    Фикс чего-то

--- Лабораторная_1/Лабораторная_1/main.cpp
+++ Лабораторная_1/Лабораторная_1/main.cpp
@@ -8,11 +8,11 @@ void main() {
 	// Загрузка записей из файла
 	container.load("input.txt");
 
-	std::cout << "В таблице " << container.size() << " элементов" << std::endl;
+	std::cout << std::endl <<  "В таблице " << container.size() << " элементов" << std::endl << std::endl;
 	// Вывод страниц
 	container.printPage(1);
 	
-	std::cout << std::endl << "Установили 2 записи на стринице" << std::endl;
+	std::cout << std::endl << "Установили 2 записи на стринице" << std::endl << std::endl;
 	container.setRowOnPage(2);
 	container.printPage(1);
 
